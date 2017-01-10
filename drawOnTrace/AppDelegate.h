92eb16e021ec2228f60c0009a640bbd2fa06e8ff
Michael Roebuck <mroebuck@control4.com>
2017-01-09 21:55:50 -0700

    Initial Commit

---
+++ drawOnTrace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  drawOnTrace
+//
+//  Created by Michael Roebuck on 9/7/16.
+//  Copyright © 2016 Michael Roebuck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ drawOnTrace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  drawOnTrace
+//
+//  Created by Michael Roebuck on 9/7/16.
+//  Copyright © 2016 Michael Roebuck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
